b8425fa0a7bd4b08d7106e86beba8c0b680a4ed0
attila kekesi <kekesi.att@gmail.com>
2020-06-10 11:56:32 +0200

    Correct analytische Faelle

--- Harm_Oszillator_01.m
+++ Harm_Oszillator_01.m
@@ -12,11 +12,16 @@ global c;
 
 vid = 0;    % als Video zu speichern: vid = 1
 
-steps = 175;
+steps = 150;
 h = 0.1;    % Zeitschritt
 m = 1;      % Gewicht
 k = 1;      % Federkoeff.
-c = 0.5;    % Daempf.
+
+%c = 0;                  % keine Daempf. (zeta = 0)
+c = 2*sqrt(m*k)*0.25;    % schwache Daempf. (0 < zeta < 1)
+%c = 2*sqrt(m*k);        % krit. Daempf. (zeta = 1)
+%c = 2*sqrt(m*k)*1.2;    % starke Daempf. (zeta > 1)
+%c = ???;                % starke & schnellste Daempf. (zeta > 1 & v0 = x0*Lambda2)
 
 x = 0;      % Anfangspos.
 v = 5;      % Anfangsgeschw.
@@ -24,18 +29,28 @@ v = 5;      % Anfangsgeschw.
 y0 = [x;v];
 Y = zeros(length(y0),steps);
 
-omega_n = sqrt(k/m);
-zeta = c/(2*m*omega_n);
-
 % Analytische Loesung
-if 0 < zeta && zeta < 1                 % schwache Daempfung
-    omega_d = omega_n*sqrt(1-zeta^2);   % csillap. rendsz. sajat koerfrekv. [rad/s]
+omega_n = sqrt(k/m);                    % csillapitatlan rendszer sajat korfrekvenciaja [rad/s]
+zeta = c/(2*m*omega_n);
+omega_d = omega_n*sqrt(1-zeta^2);       % csillap. rendsz. sajat koerfrekv. [rad/s]
+
+if c == 0                               % keine Daempfung
+    Fall = 'keine Daempf.';
+    C1 = x;
+    C2 = v/omega_n;
+    A = sqrt(x^2+v^2/omega_n^2);
+    epsilon = atan(omega_n*x/v);
+    Tn = 2*pi/omega_n;                  % periodusido [s]
+    fn = 1/Tn;                          % csillapitatlan szabad rezges sajat korfrekvenciaja [Hz = 1/s]
+    x_a = @(t) C1*cos(omega_n*t)+C2*sin(omega_n*t);
+    x_b = @(t) A*sin(omega_n*t+epsilon);
+elseif 0 < zeta && zeta < 1             % schwache Daempfung
     C1 = x;
     C2 = (v+zeta*omega_n*x)/omega_d;
     A = sqrt(C1^2+C2^2);
     epsilon = atan(C1/C2);
-    Td = 2*pi/omega_d;                  % Periodusido [s]
-    fd = 1/Td;                          % csillap rezges sajat korfrekv. [Hz = 1/s]
+    Td = 2*pi/omega_d;                   % periodusido [s]
+    f = 1/Td;                            % rezges sajat korfrekv. [Hz = 1/s]
     x_a = @(t) exp(-zeta*omega_n*t).*(C1*cos(omega_d*t)+C2*sin(omega_d*t));
     x_b = @(t) A*exp(-zeta*omega_n*t).*sin(omega_d*t+epsilon);
     x_huelle = @(t) A*exp(-zeta*omega_n*t);
@@ -51,10 +66,11 @@ elseif 1 < zeta                         % starke Daempfung
     C1 = (v-x*Lambda2)/(Lambda1-Lambda2);
     C2 = (v-x*Lambda1)/(Lambda2-Lambda1);
     x_a = @(t) C1*exp(Lambda1*t)+C2*exp(Lambda2*t);
-    Fall = '1 < \zeta';
-else
-    x_a = @(t) 0*t;
-    Fall = '\zeta < 0';
+    if v-Lambda2*x < 10^(-12)
+        Fall = '1 < \zeta & v_0 = x_0\cdot\lambda_2 \rightarrow schnellste';
+    else
+        Fall = '1 < \zeta & v_0 \neq x_0\cdot\lambda_2 \rightarrow nicht schnellste';
+    end
 end
 
 % Numerische Loesung
@@ -139,7 +155,7 @@ for n = 1:1:steps
     p11 = plot(Y(1,1),0,'o','MarkerSize',10,'LineWidth',2,'Color','k');                         % Anf.
     hold on
     p13 = plot([-L0 Y(1,n)],[0 0],'LineWidth',LW0-(Y(1,n))*LWd,'Color','#77AC30');              % Feder
-    plot([-L0 -L0],[-Max Max],'LineWidth',5,'Color','k')                                            % Wand
+    plot([-L0 -L0],[-Max Max],'LineWidth',5,'Color','k')                                        % Wand
     plot([Y(1,n) Y(1,n)],[0 kraft_d_y],'--','Color',[0.7 0.7 0.7])
     plot(Y(1,n)-geschw_dx,geschw_y,'Marker','<','LineWidth',3,'Color',[0.7 0.7 0.7])
     plot(Y(1,n)+geschw_dx,geschw_y,'Marker','>','LineWidth',3,'Color',[0.7 0.7 0.7])
@@ -179,46 +195,50 @@ for n = 1:1:steps
     p21 = plot(t,Y(1,1:n),'Color','#0072BD','LineWidth',2);
     hold on
     p22 = plot(t,Y(2,1:n),'Color','#D95319','LineWidth',2);
-    p23 = plot(t,x_a(t),'x','MarkerSize',5,'Color','k');
-    p24 = plot(t,x_b(t),'o','MarkerSize',5,'Color','k');
     xlim([0 h*steps])
     ylim([-Max Max])
     ax = gca;
     ax.XAxisLocation = 'origin';
     grid on
     grid minor
-    if contains(Fall,'0 < \zeta < 1')
+    if c == 0
+        p23 = plot(t,x_a(t),'x','MarkerSize',5,'Color','k');
+        p24 = plot(t,x_b(t),'o','MarkerSize',5,'Color','k');
+        legend([p21 p22 p23 p24],{'x (RK-4)','v (RK-4)','x (ana. a)','x (ana. b)'},'location','NorthEast')
+        text(h*steps/10,0.8*Max,['$T_n = \frac{2\pi}{\omega_n} = 2\pi\sqrt{\frac{m}{k}} =$',num2str((double(int64(Tn*100)))/100),'$s$'],'Interpreter', 'latex')
+        title(Fall,'FontSize',16,'FontWeight','normal')
+    elseif 0 < zeta && zeta < 1
+        p23 = plot(t,x_a(t),'x','MarkerSize',5,'Color','k');
+        p24 = plot(t,x_b(t),'o','MarkerSize',5,'Color','k');
         p25 = plot(t,x_huelle(t),'k');
         plot(t,-x_huelle(t),'k')
         legend([p21 p22 p23 p24 p25],{'x (RK-4)','v (RK-4)','x (ana. a)','x (ana. b)','Huelle'},'location','NorthEast')
-        text(h*steps/10,0.8*Max,['$T_d = \frac{2\pi}{\omega_d} = $',num2str((double(int64(Td*100)))/100),'$ s$'],'Interpreter', 'latex')
+        text(h*steps/10,0.8*Max,['$T_d = \frac{2\pi}{\omega_d} =$',num2str((double(int64(Td*100)))/100),'$s$'],'Interpreter', 'latex')
         title(Fall,'FontSize',16,'FontWeight','normal')
-    elseif contains(Fall,'\zeta = 1')
-        legend([p21 p22 p34 p24],{'x (RK-4)','v (RK-4)','x (ana. a)','x (ana. b)'},'location','NorthEast')
+    elseif zeta == 1
+        p23 = plot(t,x_a(t),'x','MarkerSize',5,'Color','k');
+        legend([p21 p22 p23],{'x (RK-4)','v (RK-4)','x (ana. a)'},'location','NorthEast')
         title(Fall,'FontSize',16,'FontWeight','normal')
-    elseif contains(Fall,'1 < \zeta')
-        legend([p21 p22 p34 p24],{'x (RK-4)','v (RK-4)','x (ana. a)','x (ana. b)'},'location','NorthEast')
+    elseif 1 < zeta
+        p23 = plot(t,x_a(t),'x','MarkerSize',5,'Color','k');
+        legend([p21 p22 p23],{'x (RK-4)','v (RK-4)','x (ana. a)'},'location','NorthEast')
         title(Fall,'FontSize',16,'FontWeight','normal')
-    elseif contains(Fall,'\zeta < 0')
-        legend([p21 p22 p34 p24],{'x (RK-4)','v (RK-4)','x (ana. a)','x (ana. b)'},'location','NorthEast')
-        text(h*steps/10,0.8*Max,'Noch Nicht Fertig')
-        title({Fall,' Noch Nicht Fertig'},'FontSize',16,'FontWeight','normal')
     end
     daspect([1 1 1])
     drawnow
     hold off
 
     subplot(1,3,3)
-    p30 = plot(Y(1,1),Y(2,1),'o','MarkerSize',10,'LineWidth',2,'Color','k');
+    p31 = plot(Y(1,1),Y(2,1),'o','MarkerSize',10,'LineWidth',2,'Color','k');
     hold on
-    p31 = plot(Y(1,1:n),Y(2,1:n),'k','LineWidth',2);
+    p32 = plot(Y(1,1:n),Y(2,1:n),'k','LineWidth',2);
     plot(Y(1,n),Y(2,n),'.','Color','k','Markersize',30)
     xlim([-Max Max])
     ylim([-Max Max])
     ax = gca;
     ax.XAxisLocation = 'origin';
     ax.YAxisLocation = 'origin';
-    legend([p30 p31],{'Anfang','Phasenkurve'},'location','NorthEast')
+    legend([p31 p32],{'Anfang','Phasenkurve'},'location','NorthEast')
     title('Phasenportrait','FontSize',16,'FontWeight','normal')
     xlabel('x')
     ylabel('v')
